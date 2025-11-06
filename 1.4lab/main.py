c56149a01ff54066870c456e69c87550add09899
Eduard0009 <edibro383@gmail.com>
2025-11-06 05:12:38 +0300

    Update main.py

--- 1.4lab/main.py
+++ 1.4lab/main.py
@@ -1,2 +1,10 @@
-if __name__ == "__main__":
-    pass
+lst = [5, 8, 1, 4, 10, 7, 2, 3]
+
+for x in lst:
+    if x % 2== 0:
+        print(x, end="")
+
+for x in reversed(lst):
+    if x % 2!= 0:
+        print(x, end="")
+        #4вар
