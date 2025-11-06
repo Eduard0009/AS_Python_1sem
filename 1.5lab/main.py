5549438989a82442e468f5dc590583cff6e5d9c9
Eduard0009 <edibro383@gmail.com>
2025-11-06 05:16:37 +0300

    Update main.py

--- 1.5lab/main.py
+++ 1.5lab/main.py
@@ -1,2 +1,14 @@
-if __name__ == "__main__":
-    pass
+lst = 42, 52, 85, 33, 82, 14, 164, 44335, 19, 23, 78]
+x = []
+
+for y in lst:
+    if y % 2 == 0:
+        x.append(y)
+
+for i in range(len(x)):
+    for g in range(len(x)-1):
+        if x[j] > x[g+1]:
+            x[g], x[j+1] = x[j+1],
+x[j]
+print(x)
+#4вар
