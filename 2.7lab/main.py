a9284b5fbc198c3ac2d73b4749f0aafef307fd01
Eduard0009 <edibro383@gmail.com>
2025-12-16 21:37:00 +0300

    Update main.py
    Квициния 4 вар

--- 2.7lab/main.py
+++ 2.7lab/main.py
@@ -1,2 +1,17 @@
-if __name__ == "__main__":
-    pass
+def merge_files(file1_path, file2_path):
+    with open(file1_path, 'r', encoding='utf-8') as f1, \
+         open(file2_path, 'r', encoding='utf-8') as f2:
+        lines1 = f1.readlines()
+        lines2 = f2.readlines()
+    
+    result_lines = []
+    for i in range(len(lines1)):
+        line1 = lines1[i].rstrip('\n')
+        if i < len(lines2):
+            line2 = lines2[i].rstrip('\n')
+            result_lines.append(line1 + line2)
+        else:
+            result_lines.append(line1)
+    
+    with open(file1_path, 'w', encoding='utf-8') as f1:
+        f1.write('\n'.join(result_lines) + '\n')
