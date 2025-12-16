0ee29c5ca52c25f417dc0dace88639b0975145b9
Eduard0009 <edibro383@gmail.com>
2025-12-16 21:40:07 +0300

    Update main.py
    Квициния 4 вар

--- 2.8lab/main.py
+++ 2.8lab/main.py
@@ -1,2 +1,5 @@
-if __name__ == "__main__":
-    pass
+def find_sum_min_max(filename):
+    with open(filename, 'r') as f:
+        numbers = [float(line.strip()) for line in f if line.strip()]
+    
+    return min(numbers) + max(numbers)
