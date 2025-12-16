8c0ea312baa256ceb112597b0e98ec2bcd1c36b3
Eduard0009 <edibro383@gmail.com>
2025-12-16 21:33:53 +0300

    Update main.py
    Квициния 4 вар

--- 2.5lab/main.py
+++ 2.5lab/main.py
@@ -1,2 +1,12 @@
-if __name__ == "__main__":
-    pass
+def FibRec(n):
+    global call_count
+    call_count += 1
+    if n == 1 or n == 2:
+        return 1
+    return FibRec(n - 2) + FibRec(n - 1)
+
+# 5 чисел
+for num in [5, 7, 10, 12, 15]:
+    call_count = 0
+    result = FibRec(num)
+    print(f"F({num}) = {result}, вызовов: {call_count}")
