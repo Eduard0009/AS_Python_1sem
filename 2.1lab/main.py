d98414fd03d52e6bbbce995c4b000d6ea45f61f6
Eduard0009 <edibro383@gmail.com>
2025-12-16 21:44:31 +0300

    Update main.py
    Квициния 4 вар

--- 2.1lab/main.py
+++ 2.1lab/main.py
@@ -1,2 +1,15 @@
-if __name__ == "__main__":
-    pass
+from itertools import permutations
+
+def check_key_in_dict(dictionary, keys_list):
+    return all(key in dictionary for key in keys_list)
+
+def generate_permutations(dictionary, key):
+    if key not in dictionary:
+        raise KeyError
+    value = dictionary[key]
+    if not isinstance(value, str):
+        raise ValueError
+    return [''.join(p) for p in permutations(value)]
+
+def replace_with_mean(dictionary):
+    return {k: round(sum(v) / len(v), 1) for k, v in dictionary.items() if isinstance(v, list)}
