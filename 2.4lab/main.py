58a2bb882e159a815ed798b9bb7c13f6d4beef8f
Eduard0009 <edibro383@gmail.com>
2025-12-16 21:30:39 +0300

    Update main.py
    Квициния 4 вар

--- 2.4lab/main.py
+++ 2.4lab/main.py
@@ -1,2 +1,21 @@
-if __name__ == "__main__":
-    pass
+def calculate(a, b, operation='add'):
+    if operation == 'add':
+        return a + b
+    elif operation == 'subtract':
+        return a - b
+    elif operation == 'multiply':
+        return a * b
+    elif operation == 'divide':
+        return a / b
+
+def modify_strings(strings, case=None):
+    if case == 'upper':
+        return [s.upper() for s in strings]
+    elif case == 'lower':
+        return [s.lower() for s in strings]
+    else:
+        return strings
+
+def average(*args):
+    return sum(args) / len(args) if args else 0
+
