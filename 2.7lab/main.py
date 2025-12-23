b0bbd3c993a24d02b33276dacb182df79282d872
Eduard0009 <edibro383@gmail.com>
2025-12-23 16:58:50 +0300

    Update main.py

--- 2.7lab/main.py
+++ 2.7lab/main.py
@@ -1,17 +1,18 @@
-def merge_files(file1_path, file2_path):
-    with open(file1_path, 'r', encoding='utf-8') as f1, \
-         open(file2_path, 'r', encoding='utf-8') as f2:
-        lines1 = f1.readlines()
-        lines2 = f2.readlines()
-    
-    result_lines = []
-    for i in range(len(lines1)):
-        line1 = lines1[i].rstrip('\n')
-        if i < len(lines2):
-            line2 = lines2[i].rstrip('\n')
-            result_lines.append(line1 + line2)
-        else:
-            result_lines.append(line1)
-    
-    with open(file1_path, 'w', encoding='utf-8') as f1:
-        f1.write('\n'.join(result_lines) + '\n')
+file1 = open("1.txt", "r", encoding="utf-8")
+file2 = open("2.txt", "r", encoding="utf-8")
+
+lines1 = file1.readlines()
+lines2 = file2.readlines()
+
+file1.close()
+file2.close()
+
+for i in range(len(lines1)):
+    if i < len(lines2):
+        lines1[i] = lines1[i].rstrip('\n') + lines2[i]
+
+result = open("result.txt", "w", encoding="utf-8")
+result.writelines(lines1)
+result.close()
+
+print("Готово. Результат записан в файл result.txt")
