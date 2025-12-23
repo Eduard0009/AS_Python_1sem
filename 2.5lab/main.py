f1876f2c8937436e14324297295daec3aa84e75b
Eduard0009 <edibro383@gmail.com>
2025-12-23 16:48:50 +0300

    Update main.py

--- 2.5lab/main.py
+++ 2.5lab/main.py
@@ -1,12 +1,23 @@
-def FibRec(n):
+def C(m, n):
     global call_count
     call_count += 1
-    if n == 1 or n == 2:
-        return 1
-    return FibRec(n - 2) + FibRec(n - 1)
 
-# 5 чисел
-for num in [5, 7, 10, 12, 15]:
+    if m == 0 or m == n:
+        return 1
+    else:
+        return C(m, n - 1) + C(m - 1, n - 1)
+
+
+N = int(input("Введите число N: "))
+
+M_values = []
+print("Введите 5 различных значений M:")
+for i in range(5):
+    M = int(input())
+    M_values.append(M)
+
+for M in M_values:
     call_count = 0
-    result = FibRec(num)
-    print(f"F({num}) = {result}, вызовов: {call_count}")
+    result = C(M, N)
+    print("C(", M, ",", N, ") =", result, 
+          "Количество рекурсивных вызовов:", call_count)
