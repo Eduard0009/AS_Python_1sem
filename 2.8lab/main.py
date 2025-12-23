ff2b5f5ce33a69f5ff1eb1ac267d2baa6f6bcc4f
Eduard0009 <edibro383@gmail.com>
2025-12-23 17:03:26 +0300

    Update main.py

--- 2.8lab/main.py
+++ 2.8lab/main.py
@@ -1,5 +1,20 @@
-def find_sum_min_max(filename):
-    with open(filename, 'r') as f:
-        numbers = [float(line.strip()) for line in f if line.strip()]
-    
-    return min(numbers) + max(numbers)
+file = open("f.txt", "r", encoding="utf-8")
+
+numbers = []
+for line in file:
+    numbers.append(float(line))
+
+file.close()
+
+min_value = numbers[0]
+max_value = numbers[0]
+
+for num in numbers:
+    if num < min_value:
+        min_value = num
+    if num > max_value:
+        max_value = num
+
+result = min_value + max_value
+
+print("Сумма макс и мин:", result)
