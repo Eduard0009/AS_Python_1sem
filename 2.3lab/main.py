02496265c595f1c8821169d66ce52a11cdfe0d4a
Eduard0009 <edibro383@gmail.com>
2025-12-23 16:33:19 +0300

    Update main.py

--- 2.3lab/main.py
+++ 2.3lab/main.py
@@ -1,5 +1,6 @@
 def Transp(A, M):
+    result = [[0] * M for _ in range(M)]
     for i in range(M):
-        for j in range(i + 1, M):
-            A[i][j], A[j][i] = A[j][i], A[i][j]
-    return A
+        for j in range(M):
+            result[j][i] = A[i][j]
+    return result
